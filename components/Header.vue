3ffe3fb8c1f5739585ddf0be50760917e0cceff0
ainyrihadhatul <142281874+ainyyraa125@users.noreply.github.com>
2024-02-26 16:00:41 +0700

    Add files via upload

---
+++ components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="row p-3 bg-primary text-white">
+    <div class="col-lg-1">
+      <img src="~/assets/img/LOGO-SMK4.png" alt="logo SMK 4" />
+    </div>
+
+    <div class="col-lg-11">
+      <h1>Perpus digital</h1>
+      <address>
+        SMKN 4 Tasikmalaya 
+        JL.Depok, Sukamenak, Purbaratu
+      </address>
+    </div>
+  </div>
+</template>
---
+++ pages/pengunjung/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-lg-12">
+        <h2 class="text-center my-4">RIWAYAT KUNJUNGAN</h2>
+        <div class="my-3">
+          <input type="search" class="form-control form-control-lg rounded-5" placeholder="filter..." />
+        </div>
+        <div class="my-3 text-muted">menampilkan 3 dari 3</div>
+        <table class="table">
+          <thead>
+            <tr>
+              <td>#</td>
+              <td>NAMA</td>
+              <td>KATEGORI</td>
+              <td>WAKTU</td>
+              <td>KEPERLUAN</td>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>1.</td>
+              <td>Ainy</td>
+              <td>Siswa</td>
+              <td>26 Februari 2024</td>
+              <td>Baca</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+</template>
